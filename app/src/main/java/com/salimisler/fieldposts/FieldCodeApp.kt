440a5475fac45800262d0a23b8bd2779dcf21eba
salimriskmethods <98818769+salimriskmethods@users.noreply.github.com>
2022-05-25 15:00:00 +0200

    Hilt init

---
+++ app/src/main/java/com/salimisler/fieldposts/FieldCodeApp.kt
@@ -0,0 +1,7 @@
+package com.salimisler.fieldposts
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FieldCodeApp : Application()
--- app/src/main/java/com/salimisler/fieldposts/presantation/ui/MainActivity.kt
+++ app/src/main/java/com/salimisler/fieldposts/presantation/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.salimisler.fieldposts.presantation.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.salimisler.fieldposts.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
